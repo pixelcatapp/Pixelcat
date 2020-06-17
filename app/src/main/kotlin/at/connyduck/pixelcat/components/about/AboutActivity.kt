d1b5c73d8bad70a3393c2f0e9a00ec09b2dac5d2
Konrad Pozniak <k.pozniak@gmx.at>
2020-06-17 19:07:47 +0200

    add license

--- app/src/main/kotlin/at/connyduck/pixelcat/components/about/AboutActivity.kt
+++ app/src/main/kotlin/at/connyduck/pixelcat/components/about/AboutActivity.kt
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2020 Conny Duck
+ *
+ * This file is part of Pixelcat.
+ *
+ * Pixelcat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pixelcat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package at.connyduck.pixelcat.components.about
 
 import android.content.Context
@@ -12,12 +31,10 @@ import at.connyduck.pixelcat.databinding.ActivityAboutBinding
 
 class AboutActivity : BaseActivity() {
 
-    private lateinit var binding: ActivityAboutBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityAboutBinding.inflate(layoutInflater)
+        val binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.root.setOnApplyWindowInsetsListener { _, insets ->
